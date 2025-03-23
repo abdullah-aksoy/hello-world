1f9f3545430ac0ca4b2d0bce9f27f43e5a35a710
aaksoyy <69424127+abdullah-aksoy@users.noreply.github.com>
2025-03-23 23:43:01 +0300

    Add files via upload

---
+++ hello-world/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello_world
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Merhaba, Dünya!");
+        }
+    }
+
+}
+}
